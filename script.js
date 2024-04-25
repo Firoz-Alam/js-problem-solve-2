aeb287ebe24bde3ecba6898deb60d69b51972b22
Firoz-Alam <51852342+Firoz-Alam@users.noreply.github.com>
2024-04-25 19:01:00 +0600

    problem solution two

---
+++ script.js
@@ -0,0 +1,19 @@
+// Write a function that returns the sum of two numbers.
+// Write a function that returns the sum of all numbers regardless of # of params.
+
+
+function sum(param1,param2) {
+    return param1 + param2;
+}
+
+console.log(sum(1,2))
+
+function sumOfAll(...param1){       // using Rest Operators and forEach()
+    let total=0;
+    param1.forEach(perElement => {
+        total += perElement;
+    })
+    return total;
+}
+
+console.log(sumOfAll(1,2,3,4,5,6,7,8,9,10));
